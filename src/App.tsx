25200ea52810275988f1f7ae27b2020ae80b5a0a
dhanuj kanchan <dhanujkanchan123@gmail.com>
2024-01-28 19:40:57 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Button>Click me!</Button>
+    </React.Fragment>
+  );
+}
+
+export default App;
